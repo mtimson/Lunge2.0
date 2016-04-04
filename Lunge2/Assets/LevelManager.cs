e48a9d2b49ef8f9a9b420f5e91c41669518712e3
TseringYS <tssinghe@gmail.com>
2016-04-04 12:11:40 -0400

    arena
    with weakpoints and level manager

---
+++ Lunge2/Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public int currentlevel;
+
+ 
+	public int getNumWeakPoints (int level) {
+
+        int weakPoints = level * 2;
+        return weakPoints;
+	}
+	
+    
+	
+}
